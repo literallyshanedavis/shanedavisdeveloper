8fe2ca2a8c3b7da8d77a4235bef17cbcde64905f
shanedavis <literallyshane@gmail.com>
2024-02-08 15:58:52 +0000

    vercelconfigstatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
