ba2841a7e95b03f651c549851da924e8e9f53f8f
shanedavis <literallyshane@gmail.com>
2024-02-08 14:05:04 +0000

    portfolio

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -9,7 +9,7 @@ export default function ContactButton() {
     try {
       await navigator.clipboard.writeText(email);
       setCopySuccess('Copied to clipboard!');
-      setTimeout(() => setCopySuccess(''), 2500); // Reset message after 2.5 seconds
+      setTimeout(() => setCopySuccess(''), 1000);
     } catch (err) {
       console.error("Failed to copy email: ", err);
       setCopySuccess('Failed to copy. Use Ctrl+C.');
@@ -25,18 +25,18 @@ export default function ContactButton() {
   };
 
   return (
-    <div className=''>
+    <div className='animate-fadeInDelay opacity-0'>
       <div 
         onMouseEnter={handleMouseEnter} 
         onMouseLeave={handleMouseLeave} 
-        className="group w-[270px] rounded-full border-black border-[1px] flex items-center justify-center px-8 py-4 cursor-pointer relative overflow-hidden">
-        <span className="font-medium text-lg">{copySuccess ? copySuccess : email}</span>
+        className={`group w-[270px] rounded-full border-black ${copySuccess ? 'border-0': 'border-[1px]'} flex items-center justify-center px-8 py-4 cursor-pointer relative overflow-hidden`}>
+        <span className="font-medium text-lg">{email}</span>
         {/* Hidden hover button which should overlap button above on group hover */}
         <div 
           onClick={copyToClipboard} 
-          onMouseEnter={handleMouseEnter} 
-          onMouseLeave={handleMouseLeave} 
-          className="absolute bottom-0 left-0 right-0 w-[270px] rounded-full translate-y-24 duration-500 group-hover:translate-y-0 ease-in-out transition-all border-black border-[1px] bg-black text-white flex items-center justify-center px-8 py-4 cursor-pointer">
+          // onMouseEnter={handleMouseEnter} 
+          // onMouseLeave={handleMouseLeave} 
+          className={`z-50 absolute bottom-0 left-0 right-0 w-[270px] rounded-full translate-y-24 duration-500 group-hover:translate-y-0 ease-in-out transition-all ${copySuccess ? 'bg-green-600 text-white' : 'bg-black border-black border-[1px] text-white'}  text-white flex items-center justify-center px-8 py-4 cursor-pointer`}>
           <span className="font-medium text-lg">{copySuccess ? copySuccess : "Copy to clipboard"}</span>
         </div>
       </div>
