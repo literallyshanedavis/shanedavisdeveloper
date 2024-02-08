0b75bfdfab889d9e028bc67c79c25915c4126684
shanedavis <literallyshane@gmail.com>
2024-02-08 16:01:06 +0000

    globalcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-import vercelServerless from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://shanedavis.co.uk',
   output: 'static',
-  adapter: vercelServerless(),
+  adapter: vercel(),
 });
